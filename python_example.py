6d033aecebffa1aaac47d4a6315a4f9cb3b74023
Alan <aheslop@dxc.com>
2022-02-28 15:11:34 +0000

    testing

--- python_example.py
+++ python_example.py
@@ -1,13 +1,13 @@
 # This program adds two numbers
 
-num1 = 1.5
+num1 = 1.5  
 
-num2 = 6.3
+num2 = 6.3 
 
 # Add two numbers Add two numbers Add two numbers Add two numbers Add two numbers Add  Add two numbers Add  Add two numbers Add  Add two numbers Add  Add two numbers Add  Add two numbers Add  Add two numbers Add  Add two numbers Add  Add two numbers Add  Add two numbers Add  Add two numbers Add  Add two numbers Add  Add two numbers Add  Add two numbers Add  Add two numbers Add  Add two numbers Add  Add two numbers Add  Add two numbers Add  Add two numbers Add  Add two numbers Add two numbers Add two numbers Add two numbers Add two numbers Add two numbers Add two numbers Add two numbers Add two numbers Add two numbers Add two numbers Add two numbers Add two numbers
-sum = num1 + num2
+sum = num1 + num2   
 
 
-# Display the sum22
+# Display the sum22   
 
 print("The sum of {} and {} is {}".format(num1, num2, sum))
